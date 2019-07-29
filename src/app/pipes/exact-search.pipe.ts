3035c8e4c6e6f0dc9c9e52aae16da3c039f8c6fd
Ryan D Archer <coolwebs@Ryans-MacBook-Pro.local>
2019-07-29 21:02:59 +0700

    remove old ngModels from HTML templates and add rules to custom pipe

--- src/app/pipes/exact-search.pipe.ts
+++ src/app/pipes/exact-search.pipe.ts
@@ -5,16 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExactSearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-      if (args === undefined) {
-          return value;
-      }
+  // transform(value: any, args?: any): any {
+  //     if (args === undefined) {
+  //         return value;
+  //     }
+  //
+  //     if (value.length > args) {
+  //         return value.substring(0, args) + '...';
+  //     } else {
+  //         return value;
+  //     }
+  // }
 
-      if (value.length > args) {
-          return value.substring(0, args) + '...';
-      } else {
-          return value;
-      }
-  }
+    transform(aliases: any, hashtagsSearchInput: any): any {
+        // check if search term is undefined
+        if (hashtagsSearchInput === undefined) return aliases;
+        // return updated array
+        return aliases.filter(function(alias){
+            return alias.local_part.includes(hashtagsSearchInput) || alias.domain.includes(hashtagsSearchInput);
+        });
+    }
 
 }

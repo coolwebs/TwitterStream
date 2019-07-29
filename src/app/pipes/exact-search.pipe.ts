0ff294465eac4a40da8bf1eea3db443feed35cb6
Ryan D Archer <coolwebs@Ryans-MacBook-Pro.local>
2019-07-29 22:01:55 +0700

    remove old commented out code

--- src/app/pipes/exact-search.pipe.ts
+++ src/app/pipes/exact-search.pipe.ts
@@ -5,18 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExactSearchPipe implements PipeTransform {
 
-  // transform(value: any, args?: any): any {
-  //     if (args === undefined) {
-  //         return value;
-  //     }
-  //
-  //     if (value.length > args) {
-  //         return value.substring(0, args) + '...';
-  //     } else {
-  //         return value;
-  //     }
-  // }
-
     transform(aliases: any, hashtagsSearchInput: any): any {
         // check if search term is undefined
         if (hashtagsSearchInput === undefined) return aliases;

30f8d3163e80bc7b72bc7fdd02dbbe7cd0584986
Ryan D Archer <coolwebs@Ryans-MacBook-Pro.local>
2019-07-29 09:03:23 +0700

    trim and format dates

--- src/app/hashtag-tweets/hashtag-tweets.component.ts
+++ src/app/hashtag-tweets/hashtag-tweets.component.ts
@@ -14,6 +14,13 @@ export class HashtagTweetsComponent implements OnInit {
     dataSource = new MatTableDataSource<Users>();
     displayedColumns = ['text', 'likes', 'replies', 'retweets', 'hashtags', 'date'];
 
+    convertDate(rawDate: string): string {
+        const dateOnly = rawDate.split('-')[1].trim();
+        const [day, month, year] = dateOnly.split(' ');
+        return `${month} ${day}, ${year}`;
+    }
+
+    // Setup pagination attr
     length = 50;
     pageSize = 10;
     pageSizeOptions = [5, 10, 20];
